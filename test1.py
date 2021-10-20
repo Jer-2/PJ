3b6a3c7a93cd9d9262628ff6ade95be65e54e9a1
Jer-2 <72907061+Jer-2@users.noreply.github.com>
2021-10-20 16:15:29 +0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,33 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+
+class test1(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(test1, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath =ev.msg.datapath              //封包的參數
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFMatch()
+        actions = [parser.OFPActionOutput(ofproto.FOPP_CONTROLLER,
+                                          ofproto.OFPCMLL_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
